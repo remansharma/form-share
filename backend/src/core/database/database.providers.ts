70f3175caee1bbb24a608089caff3827c460d168
remansharma <remansharma27@gmail.com>
2024-05-23 23:40:44 +0530

    added database.providers.ts

--- backend/src/core/database/database.providers.ts
+++ backend/src/core/database/database.providers.ts
@@ -1,29 +1,6 @@
-// import { Sequelize } from 'sequelize-typescript';
-// import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
-// import { databaseConfig } from './database.config';
+import { databaseConfig } from './database.config';
 
-// export const databaseProviders = [
-//   {
-//     provide: SEQUELIZE,
-//     useFactory: async () => {
-//       let config;
-//       switch (process.env.NODE_ENV) {
-//         case DEVELOPMENT:
-//           config = databaseConfig.development;
-//           break;
-//         case TEST:
-//           config = databaseConfig.test;
-//           break;
-//         case PRODUCTION:
-//           config = databaseConfig.production;
-//           break;
-//         default:
-//           config = databaseConfig.development;
-//       }
-//       const sequelize = new Sequelize(config);
-//       sequelize.addModels(['models goes here']);
-//       await sequelize.sync();
-//       return sequelize;
-//     },
-//   },
-// ];
+export const databaseProviders = () => {
+    const sequelize
+};
+
