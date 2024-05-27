4d9a94df9e70f450ecd4161fb8e381465c4c9ec5
remansharma <remansharma27@gmail.com>
2024-05-27 22:49:44 +0530

    added app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/no-auth/login/login.component';
+import { DashboardComponent } from './pages/auth/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
