afbba9201be59d34eed94b35d5036736f6850d64
remansharma <remansharma27@gmail.com>
2024-06-01 13:43:17 +0530

    added app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/no-auth/login/login.component';
 import { DashboardComponent } from './pages/auth/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+
+import { RegisterComponent } from './pages/no-auth/register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     LoginComponent,
-    DashboardComponent
+    RegisterComponent,
   ],
   imports: [
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
