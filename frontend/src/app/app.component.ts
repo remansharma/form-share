9e4307331166b0c558ea2be7a3cc700395266e35
remansharma <remansharma27@gmail.com>
2024-06-01 13:42:47 +0530

    added app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: false,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
