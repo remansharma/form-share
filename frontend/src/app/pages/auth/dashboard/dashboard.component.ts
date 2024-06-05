08956536ea89ef371e61da01241d26030868d11d
remansharma <remansharma27@gmail.com>
2024-06-05 21:29:21 +0530

    added dashboard.component.ts

--- frontend/src/app/pages/auth/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/auth/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Navigation } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-
+  createDynamicForm() {
+    console.log('Dynamic form creation');
+  }
 }
