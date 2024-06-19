f0a6734015ffc7089c996e2b609570e531e7b8bb
remansharma <remansharma27@gmail.com>
2024-06-19 21:21:07 +0530

    bug fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from './auth/auth.guard';
+// import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [{
   path: 'auth',
-  loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  canActivate: [authGuard]
+  // loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  // canActivate: [authGuard]
 }];
 
 @NgModule({
--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -3,19 +3,19 @@ import { AdminService } from '../shared/services/admin.service';
 
 import { Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+// export const authGuard: CanActivateFn = (route, state) => {
+//   return true;
 
-  constructor(
-    private _adminService: AdminService,
-    private router: Router
-  ) {}
+//   constructor(
+//     private _adminService: AdminService,
+//     private router: Router
+//   ) {}
 
-  if(this._adminService.isLoggedIn()) {
-    return true;
-  } else {
-    this.router.navigate(['/auth/login']);
-    return false;
-  }
+//   if(this._adminService.isLoggedIn()) {
+//     return true;
+//   } else {
+//     this.router.navigate(['/auth/login']);
+//     return false;
+//   }
   
-};
+// };
