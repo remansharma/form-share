2bd89bea53cd56b767731bb1860f3b72fb95c330
remansharma <remansharma27@gmail.com>
2024-06-21 23:55:49 +0530

    fixed app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './auth/auth.guard';
+import { NoAuthModule } from './pages/no-auth/no-auth.module';
 
 const routes: Routes = [{
   path: 'auth',
   loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   canActivate: [authGuard] // Use authGuard here
+},
+{
+  path: 'no-auth',
+  loadChildren: () => import('./pages/no-auth/no-auth.module').then(m => m.NoAuthModule),
+  // canActivate: [authGuard] // Use authGuard here
 }];
 
 @NgModule({
