255ecfdc8877725cfc5511685ab58a196f826e1d
remansharma <remansharma27@gmail.com>
2024-06-23 14:09:08 +0530

    added backend/src/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
+
 async function bootstrap() {
   await dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  await app.listen(80);
+  app.enableCors();
+  await app.listen(8000);
 }
 
 bootstrap();
