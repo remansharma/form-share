492950d1431bc8267797eff5f1917ee8210f0067
remansharma <remansharma27@gmail.com>
2024-06-23 14:10:10 +0530

    added frontend/src/app/app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,16 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './auth/auth.guard';
 import { NoAuthModule } from './pages/no-auth/no-auth.module';
 
-const routes: Routes = [{
-  path: 'auth',
-  loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-  canActivate: [authGuard] // Use authGuard here
-},
-{
-  path: 'no-auth',
-  loadChildren: () => import('./pages/no-auth/no-auth.module').then(m => m.NoAuthModule),
-  // canActivate: [authGuard] // Use authGuard here
-}];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [authGuard] // Use authGuard here
+  },
+  {
+    path: 'no-auth',
+    loadChildren: () => import('./pages/no-auth/no-auth.module').then(m => m.NoAuthModule),
+    // canActivate: [authGuard] // Use authGuard here
+  },
+  { path: '', redirectTo: '/no-auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/no-auth/login' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
