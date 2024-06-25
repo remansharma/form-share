3f79f831ba5332b0ff5a3ba959b4e5998a916b62
remansharma <remansharma27@gmail.com>
2024-06-25 22:37:55 +0530

    added backend/src/modules/forms/forms.service.ts

--- backend/src/modules/forms/forms.service.ts
+++ backend/src/modules/forms/forms.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Form } from './schemas/form.schema';
+import moment from 'moment';
 
 @Injectable()
 export class FormsService {
@@ -13,6 +14,36 @@ export class FormsService {
   }
 
   async findAll(): Promise<Form[]> {
-    return this.formModel.find().exec();
+
+    let listOfForms = this.formModel.find().exec();
+
+    (await listOfForms).forEach(form => {
+      let datetime = new Date(form.createdOn);
+      datetime.setHours(datetime.getHours() + 5);
+      datetime.setMinutes(datetime.getMinutes() + 30);
+      form.createdOn = datetime;
+    });
+
+    return listOfForms;
   }
+
+  async delete(id): Promise<any> {
+   let doc = await this.formModel.deleteOne({"_id": id})   
+   return {
+    message: "Successfully deleted the form",
+    data: doc
+   }
+  }
+
+  async update(id, updatedName): Promise<any> {
+    let doc = await this.formModel.findOne({"_id": id})   
+    doc.name = updatedName;
+    await doc.save()
+
+    return {
+     message: "Successfully updated the form",
+     data: doc
+    }
+   }
+
 }
