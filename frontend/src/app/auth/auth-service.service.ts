a6d1383f88bbac29a24b6e8088f10dbd1feb4805
remansharma <remansharma27@gmail.com>
2024-06-27 22:56:36 +0530

    added frontend/src/app/auth/auth-service.service.ts

--- frontend/src/app/auth/auth-service.service.ts
+++ frontend/src/app/auth/auth-service.service.ts
@@ -13,18 +13,17 @@ export class AuthService {
   isAuthenticated(): boolean {
     // Implement your logic here to check if the user is authenticated
     // For example, check if a token exists in localStorage
-    // return !!localStorage.getItem('authToken');
-    return true;
+    return !!localStorage.getItem('admin_auth');
   }
 
   // Mock method to login (this should be replaced with actual logic)
   login(token: string): void {
-    localStorage.setItem('authToken', token);
+    localStorage.setItem('admin_auth', token);
   }
 
   // Mock method to logout
   logout(): void {
-    localStorage.removeItem('authToken');
+    localStorage.removeItem('admin_auth');
   }
 
 }
