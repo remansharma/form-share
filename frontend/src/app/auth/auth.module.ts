dac488d0bc9073470acb183e0adce28bcefaa81b
remansharma <remansharma27@gmail.com>
2024-06-28 22:09:25 +0530

    added frontend/src/app/auth/auth.module.ts

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../pages/auth/dashboard/dashboard.component';
+import { CreateFormComponent } from '../pages/auth/create-form/create-form.component';
+import { ListOfFormsComponent } from '../pages/auth/list-of-forms/list-of-forms.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  // { path: 'lgo', component: HomeComponent, canActivate: [authGuard] }
+  { path: 'create-form', component: CreateFormComponent },
+  { path: 'list-of-forms', component: ListOfFormsComponent },
 ];
 
 @NgModule({
