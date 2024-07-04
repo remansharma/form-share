77c942f98613a0bd91764c4bc5988e474d360f48
remansharma <remansharma27@gmail.com>
2024-07-04 22:05:33 +0530

    added jwt.service.ts

--- frontend/src/app/pages/no-auth/services/jwt.service.ts
+++ frontend/src/app/pages/no-auth/services/jwt.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class JwtService {
 
   constructor() { }
+
+
+
+
+
+
+
+  async setAuthToken(jwtToken: string) {
+    localStorage.setItem('admin_auth', jwtToken);
+  }
+
 }
